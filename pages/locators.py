2c0e7e64f1ca9ffa0107344d288003b7fba88378
Anna <ragebluhane@gmail.com>
2021-02-11 23:09:40 +0700

    4.3 шаг 8

--- pages/locators.py
+++ pages/locators.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 
+
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
