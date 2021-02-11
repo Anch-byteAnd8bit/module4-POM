bf47b26c810b607f02b8a140f76a71095af22c87
Anna <ragebluhane@gmail.com>
2021-02-12 00:50:10 +0700

    4.3 шаг 9

--- pages/locators.py
+++ pages/locators.py
@@ -15,4 +15,9 @@ class ProductPageLocators():
 
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    # LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, ".btn-group > a.btn.btn-default")
+
+
+class BasketPageLocators():
+    TEXT_EMPTY_BASKET = (By.CSS_SELECTOR, "#content_inner > p")
+    PRODUCT_IN_BASKET = (By.CSS_SELECTOR, "#content_inner .basket_summary")
