c9007caed470405948633ae6ba38bbfaf9e4f10a
Anna <78610484+Anch-byteAnd8bit@users.noreply.github.com>
2021-02-14 01:51:08 +0700

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -37,14 +37,14 @@ class BasePage:
         except NoAlertPresentException:
             print("No second alert presented")
 
-    # элемент не появляется на странице в течение заданного времени
+    # ждем появления элемента
     def is_element_present_with_waiting(self, how, what, timeout=4):
         try:
-            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+            WebDriverWait(self.browser, timeout). \
+                until(EC.presence_of_element_located((how, what)))
         except TimeoutException:
-            return True
-
-        return False
+            return False
+        return True
 
     # элемент не появляется на странице в течение заданного времени
     def is_not_element_present(self, how, what, timeout=4):
